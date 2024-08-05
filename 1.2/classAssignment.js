6d4167255aba1b4132b1fc14cee2443ee55a8620
kushagra <goelkushagra2000@gmail.com>
2024-08-05 13:50:39 +0530

    Arrays part added

--- 1.2/classAssignment.js
+++ 1.2/classAssignment.js
@@ -26,6 +26,7 @@ for (let i = 0; i <= 1000; i++) {
 
 console.log(count);
 
+//4. Write a program that counts from 0 - 10000 and prints (for loop)
 
 
 let count1= 0; 
@@ -34,3 +35,81 @@ for (let i = 0 ;  i < 10000 ; i++ ){
 }
 
 console.log (count1);
+
+
+
+// COMPLEX PRIMITIVES - ARRAYS
+// 1. Write a program prints all the even numbers in an array 
+
+const numbers = [21,22,23,24,25];
+
+for (let i = 0; i<numbers.length ; i ++){
+
+    if (numbers[i] % 2 == 0){
+      console.log(numbers[i]);
+    }
+}
+ 
+
+//2. Write a program to print the biggest number in an arrya 
+let max = numbers[0];
+for (let i = 1 ; i<numbers.length; i++){
+  if (numbers[i] > max){
+    max = numbers[i];
+    
+  }
+
+}
+console.log("The biggest number in the array is:", max);
+
+//3.Write a program that prints all the male people’s first name given a complex object 
+
+// let personArray = ["kush", "ram", "Sejal"];
+// let genderArray = ["male", "male", "female"];
+
+// for (let i = 0 ; i< personArray.length; i++){
+//   if (genderArray[i] == "male"){
+//     console.log(personArray[i]);
+//   }
+// }
+
+// OR 
+
+const allUsers = [{
+        firstName : "Raman",
+        gender : "male"
+      },
+      {
+        firstName : "SiRaman",
+        gender : "male"
+      },
+      {
+        firstName : "Ramanika",
+        gender : "female" 
+      }]
+
+for (let i = 0 ; i < allUsers.length; i++ ){
+  if(allUsers[i]["gender"] == "male"){
+    console.log(allUsers[i]["firstName"]);
+  }
+}     
+
+
+//4. Write a program that reverses all the elements of an array
+
+let array = [1,2,3,4,5];
+let left = 0;
+let right = array.length-1;
+
+
+    while ( left < right){
+
+      let temp = array[left];
+      array[left]= array[right];
+      array[right]= temp;
+
+      left++;
+      right--;
+    }
+
+    console.log("Reversed array is ", array);
--- 1.2/main.js
+++ 1.2/main.js
@@ -22,3 +22,40 @@ if (isMarried == true) {
   console.log(firstName + " is not married");
 }
 
+
+// Arrays -> 
+
+let person1= "kush";
+let person2= "kush2";
+let person3= "kush3";
+
+//OR
+
+const personArray = ["kush", "kush1", "kush2"];
+console.log(personArray[0]);
+console.log(personArray[1]);
+console.log(personArray[2]);
+
+
+//array of objects-> 
+
+const users1 = [1,2,3];   // array of numbers
+const users2= ["raman", "kush", "sejal"] // array of strings
+
+const allUsers = [{
+          firstName : "raman",
+          gender : "male"
+      }, {
+          firstName : "Kush",
+          gender : "male"
+      }, {
+        firstName : "Kush",
+        gender : "male"
+    }
+];
+
+for (let i = 0 ; i < allUsers.length; i++ ){
+  if(allUsers[i]["gender"] == "male"){
+    console.log(allUsers[i]["firstName"]);
+  }
+}     
