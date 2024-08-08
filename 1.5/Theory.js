27e7f08f528c29ce4471917ddfd8bb6fb255aca5
kushagra <goelkushagra2000@gmail.com>
2024-08-08 19:32:08 +0530

    New video started

---
+++ 1.5/Theory.js
@@ -0,0 +1,11 @@
+// Async and sync functions 
+// Promises
+// Async Await 
+// Most important topics Need to be mugged up properly 
+
+
+
+//Synchronous functions -> Are single threaded, that means it can only be compiled line by line
+//Asynchronous functions -> Multi tasking can be done here. Multiple things are context switching
+// with each other. 
+// https://quickest-juniper-f9c.notion.site/Week-15-1-a443f3cc2f8c44fbafe76deda75fc0c0(Cohort notes)
